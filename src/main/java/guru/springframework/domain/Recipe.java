328ad596f86d8a72cdc3e95d0af480aa06f461d3
Lu Zhao <lzhao819@usc.edu>
2020-08-17 15:46:13 -0700

    Add JPA Relationship

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,10 +1,8 @@
 package guru.springframework.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
-/**
- * Created by jt on 6/13/17.
- */
 @Entity
 public class Recipe {
 
@@ -21,6 +19,10 @@ public class Recipe {
     private String directions;
     //todo add
     //private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
+
 
     @Lob
     private Byte[] image;
